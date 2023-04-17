37fc8208d6c6d06726f0261e9be62d1ee925c8e9
zhuocun <zhuocun.tang@outlook.com>
2023-04-17 22:57:08 +1000

    chore: remove unused code

--- src/main/java/zhuocun/jira_vertx_server/routes/MainRouter.java
+++ src/main/java/zhuocun/jira_vertx_server/routes/MainRouter.java
@@ -1,11 +1,11 @@
 package zhuocun.jira_vertx_server.routes;
 
-import io.vertx.ext.web.handler.JWTAuthHandler;
+// import io.vertx.ext.web.handler.JWTAuthHandler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.auth.PubSecKeyOptions;
-import io.vertx.ext.auth.jwt.JWTAuth;
-import io.vertx.ext.auth.jwt.JWTAuthOptions;
+// import io.vertx.core.buffer.Buffer;
+// import io.vertx.ext.auth.PubSecKeyOptions;
+// import io.vertx.ext.auth.jwt.JWTAuth;
+// import io.vertx.ext.auth.jwt.JWTAuthOptions;
 import io.vertx.ext.web.Router;
 
 public class MainRouter {
@@ -15,11 +15,11 @@ public class MainRouter {
     public Router create(Vertx vertx) {
         Router mainRouter = Router.router(vertx);
 
-        JWTAuthOptions jwtAuthOptions = new JWTAuthOptions().addPubSecKey(new PubSecKeyOptions()
-                .setAlgorithm("HS256").setBuffer(Buffer.buffer("your_secret_key_here")));
+        // JWTAuthOptions jwtAuthOptions = new JWTAuthOptions().addPubSecKey(new PubSecKeyOptions()
+        // .setAlgorithm("HS256").setBuffer(Buffer.buffer("your_secret_key_here")));
 
-        JWTAuth authProvider = JWTAuth.create(vertx, jwtAuthOptions);
-        JWTAuthHandler jwtAuthHandler = JWTAuthHandler.create(authProvider);
+        // JWTAuth authProvider = JWTAuth.create(vertx, jwtAuthOptions);
+        // JWTAuthHandler jwtAuthHandler = JWTAuthHandler.create(authProvider);
 
         mainRouter.route("/users*").subRouter(userRouter.create(vertx));
 
