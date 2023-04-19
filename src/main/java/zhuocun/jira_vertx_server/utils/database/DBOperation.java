c369fa4c329b961389758ceb4aa8407f583684de
zhuocun <zhuocun.tang@outlook.com>
2023-04-19 15:54:18 +1000

    chore: remove unused code

--- src/main/java/zhuocun/jira_vertx_server/utils/database/DBOperation.java
+++ src/main/java/zhuocun/jira_vertx_server/utils/database/DBOperation.java
@@ -3,7 +3,6 @@ package zhuocun.jira_vertx_server.utils.database;
 import java.util.List;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import zhuocun.jira_vertx_server.constants.MyError;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import zhuocun.jira_vertx_server.utils.database.crud.AbstractDbUtils;
@@ -19,45 +18,27 @@ public class DBOperation {
     }
 
     public Future<JsonObject> findOne(JsonObject reqBody, String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.findOne(reqBody, tableName);
     }
 
     public Future<Void> createItem(JsonObject item, String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.createItem(item, tableName);
     }
 
     public Future<List<JsonObject>> find(JsonObject reqBody, String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.find(reqBody, tableName);
     }
 
     public Future<JsonObject> findById(String id, String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.findById(id, tableName);
     }
 
     public Future<JsonObject> findByIdAndDelete(String id, String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.findByIdAndDelete(id, tableName);
     }
 
     public Future<JsonObject> findByIdAndUpdate(String id, JsonObject update,
             String tableName) {
-        if (dbUtils == null) {
-            return Future.failedFuture(MyError.DB_UTILS_NULL);
-        }
         return dbUtils.findByIdAndUpdate(id, update, tableName);
     }
 }
